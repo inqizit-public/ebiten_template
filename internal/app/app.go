34c1224bc1385d360a55697fdbfd63624e4bc697
Gaurav Sharma <gaurav.sharma@devrev.ai>
2023-05-06 18:45:36 +0530

    white screen

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,7 +8,6 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/fogleman/gg"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/audio"
 	"github.com/hajimehoshi/ebiten/v2/audio/vorbis"
@@ -16,7 +15,6 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	raudio "github.com/hajimehoshi/ebiten/v2/examples/resources/audio"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
-	"github.com/hajimehoshi/ebiten/v2/text"
 
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
@@ -83,11 +81,13 @@ func init() {
 }
 
 type Game struct {
-	touchIDs     []ebiten.TouchID
-	audioContext *audio.Context
-	jumpPlayer   *audio.Player
-	hitPlayer    *audio.Player
-	w, h         int
+	audio         Audio
+	width, height int
+}
+
+type Audio struct {
+	Context *audio.Context
+	players map[string]*audio.Player
 }
 
 func NewGame() ebiten.Game {
@@ -97,59 +97,58 @@ func NewGame() ebiten.Game {
 }
 
 func (g *Game) init() {
-	if g.audioContext == nil {
-		g.audioContext = audio.NewContext(48000)
+	if g.audio.Context == nil {
+		g.audio.Context = audio.NewContext(48000)
 	}
-
+	if g.audio.players == nil {
+		g.audio.players = map[string]*audio.Player{}
+	}
 	jumpD, err := vorbis.DecodeWithoutResampling(bytes.NewReader(raudio.Jump_ogg))
 	if err != nil {
 		log.Fatal(err)
 	}
-	g.jumpPlayer, err = g.audioContext.NewPlayer(jumpD)
+	jumpPlayer, err := g.audio.Context.NewPlayer(jumpD)
 	if err != nil {
 		log.Fatal(err)
 	}
+	g.audio.players["jump"] = jumpPlayer
 
 	jabD, err := wav.DecodeWithoutResampling(bytes.NewReader(raudio.Jab_wav))
 	if err != nil {
 		log.Fatal(err)
 	}
-	g.hitPlayer, err = g.audioContext.NewPlayer(jabD)
+	hitPlayer, err := g.audio.Context.NewPlayer(jabD)
 	if err != nil {
 		log.Fatal(err)
 	}
+	g.audio.players["hit"] = hitPlayer
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	// s := ebiten.DeviceScaleFactor()
-	// fmt.Println(outsideWidth, outsideHeight)
-	g.w = outsideWidth
-	g.h = outsideHeight
+	g.width = outsideWidth
+	g.height = outsideHeight
 	return outsideWidth, outsideHeight
-	// return screenWidth, screenHeight
-	// return int(float64(outsideWidth) * s), int(float64(outsideHeight) * s)
 }
 
 func (g *Game) Update() error {
-
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	// screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
 	screen.Fill(color.White)
-	S := 100
-	dc := gg.NewContext(S, S)
+	// S := 100
+	// dc := gg.NewContext(S, S)
 
-	dc.DrawCircle(50, 50, 50.0)
-	dc.SetRGBA255(255, 0, 0, 255)
-	dc.Fill()
-	img := ebiten.NewImageFromImage(dc.Image())
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(100, 100)
-	screen.DrawImage(img, op)
-	scoreStr := fmt.Sprintf("Something")
-	text.Draw(screen, scoreStr, arcadeFont, g.w-len(scoreStr)*fontSize, fontSize, color.Black)
+	// dc.DrawCircle(50, 50, 50.0)
+	// dc.SetRGBA255(255, 0, 0, 255)
+	// dc.Fill()
+	// img := ebiten.NewImageFromImage(dc.Image())
+	// op := &ebiten.DrawImageOptions{}
+	// op.GeoM.Translate(100, 100)
+	// screen.DrawImage(img, op)
+	// scoreStr := fmt.Sprintf("Something")
+	// text.Draw(screen, scoreStr, arcadeFont, g.width-len(scoreStr)*fontSize, fontSize, color.Black)
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.ActualTPS()))
 }
